8ed483bc93db82a1fbfcaa93ab9c8a293fa989a3
hunzzzzz <152062846+hunzzzzz@users.noreply.github.com>
2024-01-17 23:58:55 +0900

    Feat: Todo와 Comment 사이 연관관계 설정 (#14)
    - Todo와 Comment 사이에 연관관계 설정 (1:n 단방향 관계)
    - Comment 엔티티에 누락된 password 컬럼 추가
    (그에 따른 AddCommentRequest도 수정)

--- src/main/kotlin/org/hunzz/todoapplication/domain/comment/dto/request/AddCommentRequest.kt
+++ src/main/kotlin/org/hunzz/todoapplication/domain/comment/dto/request/AddCommentRequest.kt
@@ -4,7 +4,8 @@ import org.hunzz.todoapplication.domain.comment.model.Comment
 import org.hunzz.todoapplication.domain.todo.model.Todo
 
 data class AddCommentRequest(
-    val content: String
+    val content: String,
+    val password: String
 ) {
-    fun to(todo: Todo) = Comment(content, todo)
+    fun to(todo: Todo) = Comment(content, password, todo)
 }

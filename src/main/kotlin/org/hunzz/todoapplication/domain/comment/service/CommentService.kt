444b699a89ca4cf66b01c67c42ba569fea9f25f9
hunzzzzz <152062846+hunzzzzz@users.noreply.github.com>
2024-01-18 00:13:42 +0900

    Feat: Comment 추가 및 삭제 기능 구현 (#16)
    - Comment 추가 기능 구현
    - Comment 삭제 기능 구현

--- src/main/kotlin/org/hunzz/todoapplication/domain/comment/service/CommentService.kt
+++ src/main/kotlin/org/hunzz/todoapplication/domain/comment/service/CommentService.kt
@@ -1,9 +1,9 @@
 package org.hunzz.todoapplication.domain.comment.service
 
 import org.hunzz.todoapplication.domain.comment.dto.request.AddCommentRequest
-import org.hunzz.todoapplication.domain.comment.dto.response.CommentResponse
 import org.hunzz.todoapplication.domain.comment.repository.CommentRepository
 import org.hunzz.todoapplication.domain.todo.repository.TodoRepository
+import org.hunzz.todoapplication.global.exception.ModelNotFoundException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -15,7 +15,15 @@ class CommentService(
 ) {
     @Transactional
     fun addComment(todoId: Long, request: AddCommentRequest): Long {
-        val todo = todoRepository.findByIdOrNull(todoId) ?: throw Exception()
+        val todo = getTodo(todoId)
         return commentRepository.save(request.to(todo)).id!!
     }
+
+    @Transactional
+    fun deleteComment(todoId: Long, commentId: Long) {
+        getTodo(todoId)
+        commentRepository.deleteById(commentId)
+    }
+
+    private fun getTodo(todoId: Long) = todoRepository.findByIdOrNull(todoId) ?: throw ModelNotFoundException("Todo")
 }

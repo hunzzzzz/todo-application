41943fdf19202930b776189e483d64d7052760fc
hunzz <hunzz.study@gmail.com>
2024-01-17 15:46:03 +0900

    프로젝트 초기 버전 생성

---
+++ src/main/kotlin/org/hunzz/todoapplication/TodoApplication.kt
@@ -0,0 +1,11 @@
+package org.hunzz.todoapplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodoApplication>(*args)
+}

12f657aff980d7e648fab64b3bfcdef0cb6507fa
hunzz <hunzz.study@gmail.com>
2024-01-18 19:56:15 +0900

    Feat: Spring Security를 사용하기 위한 기본 세팅
    - Spring Security 라이브러리 추가
    - jjwt 관련 라이브러리 추가
    - 필터 기본 설정을 위한 SecurityConfig 클래스 추가 (global > config 패키지)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,56 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.1"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.21"
-	kotlin("plugin.spring") version "1.9.21"
-	kotlin("plugin.jpa") version "1.9.21"
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
+    kotlin("plugin.jpa") version "1.9.21"
 }
 
 group = "org.hunzz"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	// WEB
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	// DB
-	runtimeOnly("com.h2database:h2")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	// REFLECTION
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	// TEST
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	// https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
-	// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-aop
-	implementation("org.springframework.boot:spring-boot-starter-aop:3.2.1")
+    // WEB
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    // DB
+    runtimeOnly("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // REFLECTION
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // TEST
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // SWAGGER
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+    // AOP
+    implementation("org.springframework.boot:spring-boot-starter-aop:3.2.1")
+    // SECURITY
+    implementation("org.springframework.boot:spring-boot-starter-security:3.2.1")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
+    // VALIDATION
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }

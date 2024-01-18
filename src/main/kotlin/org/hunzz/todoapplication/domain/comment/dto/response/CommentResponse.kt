6d158933452bd3a4ce393155656e829859efaf8a
hunzz <hunzz.study@gmail.com>
2024-01-18 09:57:14 +0900

    Feat: Todo 목록 조회 시 하위 댓글들도 조회하는 기능 구현
    - TodoResponse의 속성에 comments 추가 (Comment 목록)
    - TodoResponse의 from 메서드가 comments도 파라미터로 받게 설정
    - TodoService에서 TodoResponse의 from을 사용하는 메서드들의 인수값 수정
    - CommentService에 FindAllCommentsByTodoId 메서드 생성
    - CommentRepository에 findAllByTodoId 메서드 생성
    - TodoService에서 CommentService를 주입받아 Comment 목록을 가져오는 getAllCommentsByTodoId 메서드 생성
    - CommentResponse에서 LocalDateTime 자료형으로 선언된 파라미터들을 yyyy-MM-dd HH:mm:ss 형식으로 변환

--- src/main/kotlin/org/hunzz/todoapplication/domain/comment/dto/response/CommentResponse.kt
+++ src/main/kotlin/org/hunzz/todoapplication/domain/comment/dto/response/CommentResponse.kt
@@ -1,12 +1,17 @@
 package org.hunzz.todoapplication.domain.comment.dto.response
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import org.hunzz.todoapplication.domain.comment.model.Comment
 import java.time.LocalDateTime
 
 data class CommentResponse(
     val content: String,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     val createdAt: LocalDateTime,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     val updatedAt: LocalDateTime
 ) {
-    fun from(comment: Comment) = CommentResponse(comment.content, comment.createdAt, comment.updatedAt)
+    companion object {
+        fun from(comment: Comment) = CommentResponse(comment.content, comment.createdAt, comment.updatedAt)
+    }
 }

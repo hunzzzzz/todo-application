8cea4f46aebf687f157bacfda8c862aa8648b058
hunzz <hunzz.study@gmail.com>
2024-01-18 11:43:41 +0900

    Feat: Method 수행 시간 측정 로직 구현
    - AOP 라이브러리 추가
    - StopWatch 어노테이션과 해당 어노테이션을 구현하는 StopWatchAspect 클래스 생성

---
+++ src/main/kotlin/org/hunzz/todoapplication/global/util/StopWatchAspect.kt
@@ -0,0 +1,29 @@
+package org.hunzz.todoapplication.global.util
+
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.util.StopWatch
+
+@Component
+@Aspect
+class StopWatchAspect {
+    private val logger = LoggerFactory.getLogger("Execution Time Logger")
+
+    @Around("@annotation(org.hunzz.todoapplication.global.util.StopWatch)")
+    fun run(joinPoint: ProceedingJoinPoint) {
+        val stopWatch = StopWatch()
+
+        stopWatch.start()
+        joinPoint.proceed()
+        stopWatch.stop()
+
+        val methodName = joinPoint.signature.name
+        val methodArguments = joinPoint.args
+
+        val timeElapsedMs = stopWatch.totalTimeMillis
+        logger.info("Method Name: $methodName | Arguments: ${methodArguments.joinToString(", ")} | Execution Time: ${timeElapsedMs}ms")
+    }
+}

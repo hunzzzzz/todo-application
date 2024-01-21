6c44164b54094d0e0f6f86ace4ecfb506a1de15d
hunzz <hunzz.study@gmail.com>
2024-01-21 11:44:08 +0900

    Bugfix: GlobalExceptionHandler 미작동 오류 해결
    - GlobalExceptionHandler가 작동하지 않는 것을 확인하고 해결

--- src/main/kotlin/org/hunzz/todoapplication/global/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/org/hunzz/todoapplication/global/exception/GlobalExceptionHandler.kt
@@ -1,19 +1,24 @@
 package org.hunzz.todoapplication.global.exception
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+    @ExceptionHandler(ModelNotFoundException::class)
     fun handleModelNotFoundException(e: ModelNotFoundException) =
         ResponseEntity.badRequest().body(e.message)
 
+    @ExceptionHandler(InvalidCriteriaException::class)
     fun handleInvalidCriteriaException(e: InvalidCriteriaException) =
         ResponseEntity.badRequest().body(e.message)
 
+    @ExceptionHandler(InvalidPasswordException::class)
     fun handleInvalidPasswordException(e: InvalidPasswordException) =
         ResponseEntity.badRequest().body(e.message)
 
+    @ExceptionHandler(InvalidCredentialException::class)
     fun handleInvalidCredentialException(e: InvalidCredentialException) =
         ResponseEntity.badRequest().body(e.message)
 }

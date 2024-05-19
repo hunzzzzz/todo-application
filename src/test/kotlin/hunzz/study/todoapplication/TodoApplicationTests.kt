e5e49dc500837a1ad7b6c5b1799ac8dba962f214
hunzz <hunzz.study@gmail.com>
2024-05-19 13:05:16 +0900

    [Setting] 프로젝트 초기 파일 업로드

---
+++ src/test/kotlin/hunzz/study/todoapplication/TodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package hunzz.study.todoapplication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
